dd22461748428d04509197f3b5786d8277263052
AndriiMed <70340848+AndriiMed@users.noreply.github.com>
2020-08-30 11:40:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+
+print("Escape sequences \n\\a \tBell (alert) \n\\b \tBackspace \n\\n \tNew Line"
+      " \n\\t \tHorizontal tab \n\\\ \tBackslash \ \n\\\" \tDouble quotation mark   \n\\\' \tSingle quotation mark  """)
+
+
+
+
+
+
+
